0c46d1470596e5019ab8415cddaf8e343189b052
Ms0232 <180540106059@darshan.ac.in>
2023-01-30 10:47:19 +0530

    nasaproject

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,44 +3,48 @@ const launches = new Map();
 let latestFlightNumber = 100;
 
 const launch = {
-    flightNumber: 100,
-    mission: 'kepler exploration X',
-    rocket: 'Explorer IS1',
-    launchData: new Date('December 27,2030'),
-    target: 'kepler-442 b',
-    customer: ['ZTM,NASA'],
-    upcoming: true,
-    success: true
-}
+  flightNumber: 100,
+  mission: "kepler exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date(),
+  target: "kepler-442 b",
+  customer: ["ZTM,NASA"],
+  upcoming: true,
+  success: true,
+};
 launches.set(launch.flightNumber, launch);
 
 function existsLaunchWithId(launchId) {
-    return launches.has(launchId)
+  return launches.has(launchId);
 }
 
 function getAllLaunches() {
-    return Array.from(launches.values())
+  return Array.from(launches.values());
 }
 
 function addNewLaunch(launch) {
-    latestFlightNumber++;
-    launches.set(
-        latestFlightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customer: ["japanvara", "amricavara"],
-            flightNumber: latestFlightNumber,
-        }));
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customer: ["japanvara", "amricavara"],
+      flightNumber: latestFlightNumber,
+    })
+  );
 }
 
 function abortLaunchById(launchId) {
-    const aborted = launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
 
 module.exports = {
-    existsLaunchWithId, getAllLaunches, addNewLaunch, abortLaunchById
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
 };
---
+++ server/src/routes/launches/launuches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("it should response with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("it should response with 200 success", () => {});
+  test("it should catch missing required properties", () => {});
+  test("it should catch invalid dates", () => {});
+});
